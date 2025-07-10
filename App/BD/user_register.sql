580ade8fad93ed26aba9598ec84da10ab172127c
carlosrcve <carlosrcve@gmail.com>
2025-07-10 12:20:30 -0400

    Add files via upload

---
+++ App/BD/user_register.sql
@@ -0,0 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-09-2022 a las 04:36:49
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ejemplo_youtube`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `login_python`
+--
+
+CREATE DATABASE IF NOT EXISTS register_user;     						
+USE register_user; 
